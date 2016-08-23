6c8c63ceb2ec3bb612820e5041c9fda863b68a78
Margarita Vasileva <m.h.vasileva@gmail.com>
2016-08-23 13:43:57 +0300

    add the templates and styling

--- hacksoft/static/js/project.js
+++ hacksoft/static/js/project.js
@@ -1,1 +1,61 @@
 /* Project specific Javascript goes here. */
+
+// here is the header picture, we have uploaded from django-cms
+var headerImage = $('.header-overlay-background img').first()
+
+if (headerImage.length !== 0) {
+  headerImage.hide();
+  // attach the picture url to the header background (uses the same css) 
+  var backgroundImage = $('.dimmed-image-background').css('background-image', "url(" + headerImage.attr('src') + ")")
+}
+
+var url = window.location.href, idx = url.indexOf("#")
+var hash = idx != -1 ? url.substring(idx + 1) : "";
+if (hash == "success") {
+  $('.popup-overlay').fadeIn();
+  $('.success-popup').fadeIn();
+}
+
+
+fixed_header = $('.header');
+
+$(window).on("scroll", function (e) {
+  if ($(document).scrollTop() > 0) {
+    fixed_header.addClass("scrolled-header");
+  } else {
+    fixed_header.removeClass("scrolled-header");
+  }
+
+});
+
+$('.homepage-header').find('.scroll-down-landpage img').click(function () {
+  var header = $('.homepage-header').height();
+  $('html, body').animate({
+    scrollTop: header
+  }, 900);
+});
+
+$('.static-header').find('.scroll-down-arrow img').click(function () {
+  var header = $('.header-image-background').height();
+  $('html, body').animate({
+    scrollTop: header
+  }, 900);
+});
+
+$('.book-a-meeting-button').click(function () {
+  $('#popup1').fadeIn();
+});
+
+$('.close').click(function () {
+  $('#popup1').fadeOut();
+  $('#success').fadeOut();
+});
+
+$(document).keydown(function (e) {
+  //ESC button pressed
+  if (e.keyCode == 27) {
+    console.log('lil');
+    $('#popup1').fadeOut();
+    $('#success').fadeOut();
+  }
+});
