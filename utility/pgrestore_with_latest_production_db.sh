d9cd4cd08e6056ac2ca6d4a02c9414d35ccf4b3b
Pavlin Gergov <gergov.pavlin@gmail.com>
2017-11-17 15:58:05 +0200

    Add script for pgrestore with latest production backup

---
+++ utility/pgrestore_with_latest_production_db.sh
@@ -0,0 +1,15 @@
+USERNAME="YOUR_USERNAME"
+
+echo "Fetching latest dump ...\n"
+curl -o db.dump `heroku pg:backups public-url --app hacksoft-website-production`
+
+echo "Recreating database ...\n"
+dropdb --if-exists hacksoft
+sudo -u postgres createdb -O $USERNAME hacksoft
+
+echo "Starting pg_restore ...\n"
+pg_restore -U $USERNAME -d hacksoft -c -j 4 db.dump
+rm db.dump
+
+echo "Migrations and data ...\n"
+python manage.py migrate
