0bfb8f2695c91b76b4cf9b055cc5053d5a279073
Radoslav Georgiev <radorado@hacksoft.io>
2018-08-16 15:57:25 +0300

    Introduce both `Rss` and `Atom` feeds

--- hacksoft/website/feed.py
+++ hacksoft/website/feed.py
@@ -1,9 +1,10 @@
 from django.contrib.syndication.views import Feed
+from django.utils.feedgenerator import Atom1Feed
 
 from hacksoft.website.snippets import HackCastEpisode
 
 
-class HackCastFeed(Feed):
+class HackCastRssFeed(Feed):
     title = 'HackCast'
     link = '/hackcast/'
     description = 'HackCast is the official HackSoft podcast.'
@@ -17,5 +18,13 @@ class HackCastFeed(Feed):
     def item_description(self, item):
         return item.description
 
+    def item_guid(self, item):
+        return item.title
+
     def item_link(self, item):
         return item.mp3_url
+
+
+class HackCastAtomFeed(HackCastRssFeed):
+    feed_type = Atom1Feed
+    subtitle = HackCastRssFeed.description
